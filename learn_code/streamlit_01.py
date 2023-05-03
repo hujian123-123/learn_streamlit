72250ad28c2883083f99b5f335ed92d4f1d692e4
Jian_Git <hujianlzj@gmail.com>
2023-05-03 20:01:37 +0800

    'hello'

--- learn_code/streamlit_01.py
+++ learn_code/streamlit_01.py
@@ -1,43 +1,34 @@
-# -*- coding:utf-8 -*-
-# @FileName  :streamlit_01.py
-# @Time      :2023/5/3 15:31
-# @Author    :Jian
-
-import numpy as np
-import altair as alt
-import pandas as pd
 import streamlit as st
 
-st.header('st.write')
+st.title('🖼️ yt-img-app')
+st.header('YouTube Thumbnail Image Extractor App')
 
-# 样例 1
+with st.expander('About this app'):
+    st.write('This app retrieves the thumbnail image from a YouTube video.')
 
-st.write('Hello, *World!* :sunglasses:')
+# Image settings
+st.sidebar.header('Settings')
+img_dict = {'Max': 'maxresdefault', 'High': 'hqdefault', 'Medium': 'mqdefault', 'Standard': 'sddefault'}
+selected_img_quality = st.sidebar.selectbox('Select image quality', ['Max', 'High', 'Medium', 'Standard'])
+img_quality = img_dict[selected_img_quality]
 
-# 样例 2
+yt_url = st.text_input('Paste YouTube URL', 'https://youtu.be/JwSS70SZdyM')
 
-st.write(1234)
-code = '''def hello():
-    print("Hello, Streamlit!")'''
-st.code(code, language='python')
 
-# 样例 3
+def get_ytid(input_url):
+    if 'youtu.be' in input_url:
+        ytid = input_url.split('/')[-1]
+    if 'youtube.com' in input_url:
+        ytid = input_url.split('=')[-1]
+    return ytid
 
-df = pd.DataFrame({
-     'first column': [1, 2, 3, 4],
-     'second column': [10, 20, 30, 40]
-     })
-st.write(df)
 
-# 样例 4
+# Display YouTube thumbnail image
+if yt_url != '':
+    ytid = get_ytid(yt_url)  # yt or yt_url
 
-st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
-
-# 样例 5
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
+    yt_img = f'http://img.youtube.com/vi/{ytid}/{img_quality}.jpg'
+    st.image(yt_img)
+    st.write('YouTube video thumbnail image URL: ', yt_img)
+else:
+    st.write('☝️ Enter URL to continue ...')
